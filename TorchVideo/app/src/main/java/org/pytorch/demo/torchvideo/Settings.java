2ed668434b7698d9b94bd6f1bc3ec9ba2fe83aa2
vescrity <vescrity@foxmail.com>
2025-04-27 16:53:57 +0800

    update

--- TorchVideo/app/src/main/java/org/pytorch/demo/torchvideo/Settings.java
+++ TorchVideo/app/src/main/java/org/pytorch/demo/torchvideo/Settings.java
@@ -9,5 +9,8 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        findViewById(R.id.returnButton).setOnClickListener(v -> finish());
+
     }
 }
