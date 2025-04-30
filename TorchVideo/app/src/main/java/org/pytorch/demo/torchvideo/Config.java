b4b27ffec02dc6de443fbe134a6da12d0425879b
vescrity <vescrity@foxmail.com>
2025-04-30 15:01:31 +0800

    Add Config Class

---
+++ TorchVideo/app/src/main/java/org/pytorch/demo/torchvideo/Config.java
@@ -0,0 +1,20 @@
+package org.pytorch.demo.torchvideo;
+
+public class Config {
+    private static Config instance;
+    public String ptlPath = "Default";
+
+    private Config() {}
+
+    // 获取单例实例
+    public static synchronized Config getInstance() {
+        if (instance == null) {
+            instance = new Config();
+        }
+        return instance;
+    }
+
+    public void someMethod() {
+        // 方法实现
+    }
+}
